58c20161e7d15afa8cd052e350c19d6b1021a51e
mrl5 <jakuboo+github@gmail.com>
2018-10-01 19:01:56 +0200

    worker.py: switched to OOP

--- test/test_worker.py
+++ test/test_worker.py
@@ -3,20 +3,20 @@
 import pytest
 import subprocess
 import os
-from worker import evaluate
+from worker import Worker
 
 __author__ = "mrl5"
 
 """
 Scenario:
-    - 'evaluate()' function should return expected values
-    - 'evaluate()' function should ignore extra whitespaces
-    - test if 'evaluate()' function is run when worker.py is executed in console `python worker.py`
+    - 'evaluate()' method should return expected values
+    - 'evaluate()' method should ignore extra whitespaces
+    - test if 'evaluate()' method is run when worker.py is executed in console `python worker.py`
     - worker.py accepts only one argument else raise "Exception" exception
-    - 'evaluate()' function should rise "IndexError" exception when there are too many operators in RPN expression
-    - 'evaluate()' function should rise "CorruptedRPNExpression" custom exception if list is not empty after evaluation
-    - 'evaluate()' function should rise "EmptyInput" custom exception if expression is empty
-    - 'evaluate()' function should rise "ValueError" when expression has other strings than defined in 'ops' dictionary
+    - 'evaluate()' method should rise "IndexError" exception when there are too many operators in RPN expression
+    - 'evaluate()' method should rise "CorruptedRPNExpression" custom exception if list is not empty after evaluation
+    - 'evaluate()' method should rise "EmptyInput" custom exception if expression is empty
+    - 'evaluate()' method should rise "ValueError" when expression has other strings than defined in 'ops' dictionary
 """
 
 
@@ -32,16 +32,21 @@ def expected_results():
     return dictionary
 
 
-def test_evaluate(expected_results):
+@pytest.fixture(scope="function")
+def worker_instance():
+    worker = Worker()
+    return worker
+
+def test_evaluate_method(expected_results, worker_instance):
     real_results = {}
     # create dictionary with real results
     for key, value in expected_results.items():
-        result = {key: evaluate(key)}
+        result = {key: worker_instance.evaluate(key)}
         real_results.update(result)
     assert expected_results == real_results
 
 
-def test_additional_whitespaces_in_expression(expected_results):
+def test_additional_whitespaces_in_expression(expected_results, worker_instance):
     # add extra expressions to the expected_results dict
     expected_results.update({"3 4 + ": 7})
     expected_results.update({"3  4 +": 7})
@@ -53,16 +58,16 @@ def test_additional_whitespaces_in_expression(expected_results):
     expected_results.update({"3\t4\t+": 7})
     real_results = {}
     for key, value in expected_results.items():
-        result = {key: evaluate(key)}
+        result = {key: worker_instance.evaluate(key)}
         real_results.update(result)
     assert expected_results == real_results
 
 
-def test_shell_subprocess():
+def test_shell_subprocess(worker_instance):
     test_file_location = os.path.dirname(os.path.realpath(__file__))
     tested_file = os.path.join(os.path.dirname(test_file_location), "worker.py")
     expression = "2 2 +"
-    result = evaluate(expression)
+    result = worker_instance.evaluate(expression)
     stdout_expected_result = str.encode(str(result) + "\n")
     stdout = subprocess.check_output(["python", tested_file, expression])
     assert stdout == stdout_expected_result
@@ -83,7 +88,7 @@ def test_shell_subprocess_with_no_args():
         subprocess.check_call(["python", tested_file])
 
 
-def test_for_IndexError():
+def test_for_IndexError(worker_instance):
     corrupted_expression = "3 4 + -"
     with pytest.raises(IndexError):
-        evaluate(corrupted_expression)
+        worker_instance.evaluate(corrupted_expression)
--- worker.py
+++ worker.py
@@ -1,42 +1,40 @@
 # -*- coding: utf-8 -*-
 
 # based on Yehonathan Sharvit blog post: https://blog.klipse.tech/python/2016/09/22/python-reverse-polish-evaluator.html
-
-# loaded from gist: https://gist.github.com/viebel/3d0f146484989b0c5afc29e53e3e9f2c
-# This example assumes binary operators, but this is easy to extend.
-# Comes from this excellent article: http://blog.reverberate.org/2013/07/ll-and-lr-parsing-demystified.html
-
-# arithmetic operators as lambda functions (anonymous functions)
-ops = {
-  "+": (lambda a, b: a + b),
-  "-": (lambda a, b: a - b),
-  "*": (lambda a, b: a * b),
-  "/": (lambda a, b: a / b)
-}
+__author__ = "mrl5"
 
 
-def evaluate(expression):
-    tokens = expression.split()
-    stack = []
+class Worker:
+    def __init__(self):
+        # arithmetic operators as lambda functions (anonymous functions)
+        self._ops = {
+            "+": (lambda a, b: a + b),
+            "-": (lambda a, b: a - b),
+            "*": (lambda a, b: a * b),
+            "/": (lambda a, b: a / b)
+        }
 
-    for token in tokens:
-        if token in ops:
-            number2 = stack.pop()
-            number1 = stack.pop()
-            result = ops[token](number1, number2)
-            stack.append(result)
-        else:
-            stack.append(int(token))
-    return stack.pop()
+    def evaluate(self, expression):
+        tokens = expression.split()
+        stack = []
+        for token in tokens:
+            if token in self._ops:
+                number2 = stack.pop()
+                number1 = stack.pop()
+                result = self._ops[token](number1, number2)
+                stack.append(result)
+            else:
+                stack.append(int(token))
+        return stack.pop()
 
 
 def main():
     allowed_args = 2 if sys.argv[0] == __file__ else 1
     if len(sys.argv) != allowed_args:
         raise Exception("Script accepts only one argument.")
-
     expression = sys.argv[1] if sys.argv[0] == __file__ else sys.argv[0]
-    print(evaluate(expression))
+    worker = Worker()
+    print(worker.evaluate(expression))
 
 
 if __name__ == "__main__":
