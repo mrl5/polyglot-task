4e64d553b2c96ebc27342e70d57228dad83de9e6
mrl5 <jakuboo+github@gmail.com>
2018-10-01 16:44:45 +0200

    "evaluate()" function (used Yehonathan Sharvit algorithm)

--- worker.py
+++ worker.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 
+# based on Yehonathan Sharvit blog post: https://blog.klipse.tech/python/2016/09/22/python-reverse-polish-evaluator.html
+
+# loaded from gist: https://gist.github.com/viebel/3d0f146484989b0c5afc29e53e3e9f2c
+# This example assumes binary operators, but this is easy to extend.
+# Comes from this excellent article: http://blog.reverberate.org/2013/07/ll-and-lr-parsing-demystified.html
+
+# arithmetic operators as lambda functions (anonymous functions)
+ops = {
+  "+": (lambda a, b: a + b),
+  "-": (lambda a, b: a - b),
+  "*": (lambda a, b: a * b),
+  "/": (lambda a, b: a / b)
+}
+
 
 def evaluate(expression):
-    pass
+    tokens = expression.split()
+    stack = []
+
+    for token in tokens:
+        if token in ops:
+            number2 = stack.pop()
+            number1 = stack.pop()
+            result = ops[token](number1, number2)
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    return stack.pop()
