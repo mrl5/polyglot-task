a924339f4caa6ec5bd48088b7e75e9d69dcc489e
mrl5 <jakuboo+github@gmail.com>
2018-10-01 16:56:37 +0200

    test case: additional whitespaces in expression

--- test/test_worker.py
+++ test/test_worker.py
@@ -16,16 +16,38 @@ Scenario:
 """
 
 
-def test_evaluate():
-    expected_results = {
+@pytest.fixture(scope="function")
+def expected_results():
+    dictionary = {
         "3 4 +": 7,
         "5 1 2 + 4 * + 3 -": 14,
-        "1 2 +" : 3,
+        "1 2 +": 3,
         "990 1 2 + *": 2970,
         "1000 990 1 2 + * +": 3970
     }
+    return dictionary
+
+
+def test_evaluate(expected_results):
     real_results = {}
     # create dictionary with real results
+    for key, value in expected_results.items():
+        result = {key: evaluate(key)}
+        real_results.update(result)
+    assert expected_results == real_results
+
+
+def test_additional_whitespaces_in_expression(expected_results):
+    # add extra expressions to the expected_results dict
+    expected_results.update({"3 4 + ": 7})
+    expected_results.update({"3  4 +": 7})
+    expected_results.update({"3 4  +": 7})
+    expected_results.update({" 3 4  +": 7})
+    expected_results.update({"3 4 +\t": 7})
+    expected_results.update({"3 \t4 +": 7})
+    expected_results.update({"\t3 4 + ": 7})
+    expected_results.update({"3\t4\t+": 7})
+    real_results = {}
     for key, value in expected_results.items():
         result = {key: evaluate(key)}
         real_results.update(result)
