27f1023907342f9383a7e8cca3d051cde707e9f9
mrl5 <jakuboo+github@gmail.com>
2018-10-01 22:05:48 +0200

    basic input verification

--- endpoint.rb
+++ endpoint.rb
@@ -11,3 +11,15 @@ puts ""
 puts "If you change your mind, hit Ctrl+C to end this app"
 puts ""
 puts "Your input:"
+
+inp = gets.chomp
+no_of_expressions = inp.to_i
+if no_of_expressions.is_a? Integer
+	if no_of_expressions > 0
+		puts "Hurray"
+	else
+		abort("Number of RPN expressions must be greather than 0. Aborting.")
+	end
+else
+	abort("First line must be an integer. Aborting.")
+end
