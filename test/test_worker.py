7de4aab25f0b01ba85cbdeeeb9b46376b3fee710
mrl5 <jakuboo+github@gmail.com>
2018-10-01 16:42:16 +0200

    test case: function returns expected values

--- test/test_worker.py
+++ test/test_worker.py
@@ -8,8 +8,25 @@ __author__ = "mrl5"
 """
 Scenario:
     - 'evaluate()' function should return expected values
+    - 'evaluate()' function should ignore extra whitespaces
     - 'evaluate()' function should rise "IndexError" exception when there are too many operators in RPN expression
     - 'evaluate()' function should rise "CorruptedRPNExpression" custom exception if list is not empty after evaluation
     - 'evaluate()' function should rise "EmptyInput" custom exception if expression is empty
     - 'evaluate()' function should rise "ValueError" when expression has other strings than defined in 'ops' dictionary
 """
+
+
+def test_evaluate():
+    expected_results = {
+        "3 4 +": 7,
+        "5 1 2 + 4 * + 3 -": 14,
+        "1 2 +" : 3,
+        "990 1 2 + *": 2970,
+        "1000 990 1 2 + * +": 3970
+    }
+    real_results = {}
+    # create dictionary with real results
+    for key, value in expected_results.items():
+        result = {key: evaluate(key)}
+        real_results.update(result)
+    assert expected_results == real_results
