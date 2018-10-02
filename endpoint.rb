90f1a4da1c0a0a391a00c3d9cf37586e6ff37f55
mrl5 <jakuboo+github@gmail.com>
2018-10-02 14:28:31 +0200

    more elegant syntax

--- endpoint.rb
+++ endpoint.rb
@@ -6,23 +6,28 @@ api_name = "worker.py"
 # global variable: path to API
 $api = File.join(script_path, api_name)
 
-puts "Reverse Polish notation app"
-puts ""
-puts "Input syntax:"
-puts "\tNumber of RPN expressions"
-puts "\t1st RPN expression"
-puts "\t2nd RPN expression"
-puts "\t..."
-puts "\tNth RPN expression"
-puts ""
-puts "If you change your mind, hit Ctrl+C to end this app"
-puts ""
-puts "Input:"
+def welcome_message()
+	puts "Reverse Polish notation app"
+	puts ""
+	puts "Input syntax:"
+	puts "\tNumber of RPN expressions"
+	puts "\t1st RPN expression"
+	puts "\t2nd RPN expression"
+	puts "\t..."
+	puts "\tNth RPN expression"
+	puts ""
+	puts "If you change your mind, hit Ctrl+C to end this app"
+	puts ""
+	puts "Input:"
+end
 
-# save input and (.chomp) remove carriage return from end of the input string
-inp = gets.chomp
-# convert input to integer, if inp is a string result will be 0
-no_of_expressions = inp.to_i
+def init_func()
+	# save input and (.chomp) remove carriage return from end of the input string
+	inp = gets.chomp
+	# convert input to integer, if inp is a string result will be 0
+	no_of_expressions = inp.to_i
+	return no_of_expressions
+end
 
 def get_RPN_expressions(no_of_exp)
 	expressions = Array.new
@@ -40,6 +45,8 @@ def pass_to_api(arguments)
 end
 
 # main
+welcome_message
+no_of_expressions = init_func
 if no_of_expressions.is_a? Integer
 	if no_of_expressions > 0
 		expressions = get_RPN_expressions(no_of_expressions)
