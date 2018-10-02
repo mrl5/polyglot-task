471327dae918a54a56b38bb8943183d4cd628998
mrl5 <jakuboo+github@gmail.com>
2018-10-02 15:17:19 +0200

    working with command-line aguments: path to the binary version of executed program

--- api.go
+++ api.go
@@ -1,7 +1,12 @@
 package main
 
+import "os"
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	/* path to binary version of this program */
+	pathToProgram := os.Args[0]
+	//commandLineArgs := os.Args[1:]
+
+	fmt.Println("Path: " + pathToProgram)
 }
