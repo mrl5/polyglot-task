8fbeedad1dba43e0eeff3d3d21fe06a221db37f4
mrl5 <jakuboo+github@gmail.com>
2018-10-02 17:22:36 +0200

    create logfiles if they dont exist

--- api.go
+++ api.go
@@ -6,8 +6,15 @@ import (
 	"errors"
 	"path/filepath"
 	"os/user"
+	"path"
 )
 
+var logsDir = ""
+const APP_DIR string = "polyglot-task-by-JK-aka-mrl5"
+const INPUT_LOG_FILE string = "input.log"
+const OUTPUT_LOG_FILE string = "output.log"
+const APPDIR_PERMISSION = 0750 //owner can do anything with directory, group member can list directory and see rights, others wont see dir content
+const LOGFILES_PERMISSION = 0640 //RW for owner, R for group, nothing for others (0640)
 
 func checkInput() (int, error) {
 	switch len(os.Args) {
@@ -26,19 +33,41 @@ func checkError(e error) {
 	}
 }
 
+func checkEnvironment(usrDir string) {
+	if logsDir == "" {
+		logsDir = path.Join(usrDir, APP_DIR)
+	}
+	pathToInputLog := path.Join(logsDir, INPUT_LOG_FILE)
+	pathToOutputLog := path.Join(logsDir, OUTPUT_LOG_FILE)
+
+	/* create logs dir if doesn't exist, ignore error when exists */
+	_ = os.Mkdir(logsDir, APPDIR_PERMISSION)
+
+	/* check if log files exists */
+	logFileExists(pathToInputLog)
+	logFileExists(pathToOutputLog)
+}
+
+func logFileExists(pathToFile string) {
+	/* checks if file exists */
+	os.OpenFile(pathToFile, os.O_RDONLY|os.O_CREATE, LOGFILES_PERMISSION)
+}
+
 func main() {
 	/* path to directory of binary version of this program */
-	workDir, werr := filepath.Abs(filepath.Dir(os.Args[0]))
-	checkError(werr)
-	usrEnv, uerr := user.Current()
-	checkError(uerr)
+	workDir, workDirErr := filepath.Abs(filepath.Dir(os.Args[0]))
+	checkError(workDirErr)
+	usrEnv, usrEnvErr := user.Current()
+	checkError(usrEnvErr)
+
+	checkEnvironment(usrEnv.HomeDir)
 
 	/* check input syntax */
-	if _, argerr := checkInput(); argerr == nil {
+	if _, argErr := checkInput(); argErr == nil {
 		fmt.Println("Work dir: " + workDir)
-		fmt.Println("User dir: " + usrEnv.HomeDir)
+		fmt.Println("Logs dir: " + logsDir)
 		fmt.Println("Arg: " + os.Args[1])
 	} else {
-		fmt.Println(argerr)
+		fmt.Println(argErr)
 	}
 }
