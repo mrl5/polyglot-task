c08bce51a6df8978f34ea015034da4f5858b61ce
mrl5 <jakuboo+github@gmail.com>
2018-10-02 15:37:30 +0200

    iterate array kata

--- api.go
+++ api.go
@@ -1,12 +1,19 @@
 package main
 
-import "os"
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"os"
+)
 
 func main() {
 	/* path to binary version of this program */
 	pathToProgram := os.Args[0]
-	//commandLineArgs := os.Args[1:]
-
 	fmt.Println("Path: " + pathToProgram)
+
+	if len(os.Args) > 1 {
+		for i := 1; i < len(os.Args); i++ {
+			fmt.Println("Arg " + strconv.Itoa(i) + ": "+ os.Args[i])
+		}
+	}
 }
