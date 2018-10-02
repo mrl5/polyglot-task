4695e3e06b3966f1afd903ca5d140734d0d05f1e
mrl5 <jakuboo+github@gmail.com>
2018-10-02 16:04:16 +0200

    working with errors kata

--- api.go
+++ api.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"os"
+	"errors"
 )
+
+func checkInput() (int, error) {
+	switch len(os.Args) {
+	case 2:
+		return 0, nil
+	case 1:
+		return -1, errors.New("error: expected an argument")
+	default:
+		return -2, errors.New("error: only one argument is accepted")
+	}
+}
 
 func main() {
 	/* path to binary version of this program */
 	pathToProgram := os.Args[0]
 	fmt.Println("Path: " + pathToProgram)
 
-	if len(os.Args) > 1 {
-		for i := 1; i < len(os.Args); i++ {
-			fmt.Println("Arg " + strconv.Itoa(i) + ": "+ os.Args[i])
-		}
+	if r, e := checkInput(); e == nil {
+		fmt.Println("Arg: " + os.Args[1])
+	} else {
+		fmt.Println("return code: ", r)
+		fmt.Println(e)
 	}
 }
