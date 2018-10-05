2c900a709698c045e68b3b33fb4ab2e1f7e5213d
mrl5 <jakuboo+github@gmail.com>
2018-10-05 13:31:17 +0200

    accept endpoint uuid as a flag

--- api.go
+++ api.go
@@ -11,25 +11,54 @@ import (
 	"time"
 	"strconv"
 	"strings"
+	"flag"
 )
 
-const WORKER = "worker.py"
-const APP_DIR string = "polyglot-task-by-JK-aka-mrl5"
-const INPUT_LOG_FILE string = "input.log"
-const APPDIR_PERMISSION = 0750 //owner can do anything with directory, group member can list directory and see rights, others wont see dir content
-const LOGFILES_PERMISSION = 0640 //RW for owner, R for group, nothing for others (0640)
-const ERROR_MSG = "error"
+/* worker related constants */
+const (
+	WORKER = "worker.py"
+	ERROR_MSG = "error"
+)
+
+/* logging related constants */
+const (
+	APP_DIR = "polyglot-task-by-JK-aka-mrl5"
+	INPUT_LOG_FILE = "input.log"
+	APPDIR_PERMISSION = 0750   //owner can do anything with directory, group member can list directory and see rights, others wont see dir content
+	LOGFILES_PERMISSION = 0640 //RW for owner, R for group, nothing for others (0640)
+)
+
+var endpointUUID string
 var logsDir = ""
 var pathToInputLog string
 
+func setFlags() {
+	/* flags related constants */
+	const (
+		DEFAULT_UUID = "internal"
+		UUID_USAGE = "Universally Unique IDentifier of an endpoint request"
+	)
+
+	/* UUID */
+	flag.StringVar(&endpointUUID, "uuid", DEFAULT_UUID, UUID_USAGE)
+	flag.StringVar(&endpointUUID, "u", DEFAULT_UUID, UUID_USAGE+" (shorthand)")
+
+	/* parse all defined flags */
+	flag.Parse()
+}
+
 func checkInput() (int, error) {
-	switch len(os.Args) {
-	case 2:
-		return 0, nil
+	fmt.Println(os.Args)
+	fmt.Println(len(flag.Args()))
+	fmt.Println(flag.Args())
+	fmt.Println(endpointUUID)
+	switch len(flag.Args()) {
 	case 1:
-		return -1, errors.New("error: expected an argument")
+		return 0, nil
+	case 0:
+		return -1, errors.New("API error: expected an argument")
 	default:
-		return -2, errors.New("error: only one argument is accepted")
+		return -2, errors.New("API error: only one argument is accepted")
 	}
 }
 
@@ -53,9 +82,7 @@ func checkEnvironment(usrDir string) {
 }
 
 func checkForFile(pathToFile string) {
-	/** checks if file exists
-	 *  creates when doesn't
-	 */
+	/* checks if file exists, creates when doesn't */
 	os.OpenFile(pathToFile, os.O_RDONLY|os.O_CREATE, LOGFILES_PERMISSION)
 }
 
@@ -78,19 +105,20 @@ func executeWorker(pathToWorker string, argument string) (error, []byte) {
 func main() {
 	start := time.Now()
 	var elapsedTime string
-	/* path to directory of binary version of this program */
+	setFlags()
+
+	/* path to the directory of binary version of this program */
 	workDir, workDirErr := filepath.Abs(filepath.Dir(os.Args[0]))
 	checkError(workDirErr)
 
 	usrEnv, usrEnvErr := user.Current()
 	checkError(usrEnvErr)
-
 	checkEnvironment(usrEnv.HomeDir)
 
 	/* check input syntax */
 	if _, argErr := checkInput(); argErr == nil {
 		pathToWorker := path.Join(workDir, WORKER)
-		err, result := executeWorker(pathToWorker, os.Args[1])
+		err, result := executeWorker(pathToWorker, flag.Arg(0))
 		execTime := time.Since(start).Seconds()
 		elapsedTime = strconv.FormatFloat(execTime, 'f', 3, 64)
 		if err != nil {
