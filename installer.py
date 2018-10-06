43258f60c3947d50c695f10017f91fce206c33be
mrl5 <jakuboo+github@gmail.com>
2018-10-06 19:59:36 +0200

    test case: python major version is the same as declared in dependencies

--- installer.py
+++ installer.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import sys
 
 __author__ = "mrl5"
 
+
+class Installer:
+    """
+    Class for installer
+    """
+
+    def __init__(self):
+        self._dependencies = {
+            "python": {"version": 3, "present": sys.version_info.major},
+            "ruby": {"version": ["2.3", "2.4"], "present": None},
+            "go": {"version": "1.10", "present": None}
+        }
+
+    def _verify_python(self, sys_version_major):
+        pass
--- test/test_installer.py
+++ test/test_installer.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from installer import Installer
 
 __author__ = "mrl5"
 
 """
 Scenario:
-    - 'verify_python()' should return true if version 3
-    - 'verify_python()' should return false if other version
-    - 'verify_ruby()' should return true if version 2.4
-    - 'verify_ruby()' should return true if version 2.3
-    - 'verify_ruby()' should return false if other version
-    - 'verify_go()' should return true if version 1
-    - 'verify_go()' should return false if other version
-    - 'verify_dir_access()' should return true if project's dir is writable
-    - 'verify_dir_access()' should return false if project's dir is not writable
-    - 'compile_api()' should compile 'api.go'
+    - '_verify_python()' should return true if version 3
+    - '_verify_python()' should return false if other version
+    - '_verify_ruby()' should return true if version 2.4
+    - '_verify_ruby()' should return true if version 2.3
+    - '_verify_ruby()' should return false if other version
+    - '_verify_go()' should return true if version 1.10
+    - '_verify_go()' should return false if other version 
+    - '_compile_api()' should compile 'api.go'
     - binary file created by 'compile_api()' should be in project's directory
     - 'verify_permission()' should return true if file is executable
     - 'verify_permission()' should return false if file is not executable 
@@ -23,3 +22,14 @@ Scenario:
     - 'set_executable_permission()' should throw 'OSError' when permission can't be set
     - 'install()' should create a project with right structure
 """
+
+
+@pytest.fixture(scope="function")
+def installer_instance():
+    installer = Installer()
+    return installer
+
+
+def test_verify_python_success(installer_instance):
+    python_version_major = installer_instance._dependencies["python"]["version"]
+    assert installer_instance._verify_python(python_version_major) is True
