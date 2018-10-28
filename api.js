96d27b4793cbe5c491f654c519e120fb8ef26553
mrl5 <jakuboo+github@gmail.com>
2018-10-28 21:41:41 +0100

    measure total time

--- api.js
+++ api.js
@@ -1,25 +1,36 @@
 #!/usr/bin/env node
+var startTime = new Date();
 var spawn = require('child_process').spawn;
 
-function callWorker(argument, id) {
+function callWorker(argument, id, noOfArgs) {
     let processStart = new Date();
     let workerCmd = "./worker.py";
     let worker = spawn(workerCmd, [argument]);
     worker.stdout.on('data', (data) => {
         elapsedTime = new Date() - processStart;
-        console.log(`${id}: ${data.toString().trim()}, ${elapsedTime/1000}`);
+        console.log(`${id}: ${data.toString().trim()}, ${elapsedTime / 1000}`);
     });
     worker.stderr.on('data', (data) => {
         elapsedTime = new Date() - processStart;
-        console.error(`${id}: error, ${elapsedTime/1000}`);
+        console.error(`${id}: error, ${elapsedTime / 1000}`);
     });
+    worker.on('close', () => {
+        if (id + 1 === noOfArgs) {
+            getTotalTime();
+        }
+    });
+}
+
+function getTotalTime() {
+    let totalTime = new Date() - startTime;
+    console.log(`total time: ${totalTime / 1000}`);
 }
 
 if (process.argv.length > 2) {
     let expressions = process.argv.slice(2);
     expressions.forEach((expression, index) => {
         console.log(`${index}: ${expression}`);
-        callWorker(expression, index);
+        callWorker(expression, index, expressions.length);
     });
 } else {
     console.error("API error: expected at least one argument");
