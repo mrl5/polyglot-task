a81f934e6470cfd4ef29ccf717a85498c32045e4
mrl5 <jakuboo+github@gmail.com>
2018-10-28 20:42:21 +0100

    asynchronously call worker.py and measure worker.py execution time

--- api.js
+++ api.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
+var spawn = require('child_process').spawn;
+
+function callWorker(argument) {
+    let id = Symbol(argument);
+    console.time(id.toString());
+    let workerCmd = "./worker.py";
+    let worker = spawn(workerCmd, [argument]);
+    worker.stdout.on('data', (data) => {
+        console.log(`${argument}: ${data.toString()}`);
+    });
+    worker.stderr.on('data', (data) => {
+        console.error(`${argument}: error`);
+    });
+    worker.on('close', () => {
+        console.timeEnd(id.toString());
+    });
+
+}
 
 if (process.argv.length > 2) {
     let expressions = process.argv.slice(2);
     expressions.forEach((expression) => {
-        console.log(expression);
+        callWorker(expression);
     });
 } else {
     console.error("API error: expected at least one argument");
