092507246431988c712e7e9d1c6e50ad313b7c7d
mrl5 <jakuboo+github@gmail.com>
2018-10-28 21:06:44 +0100

    track order, measure time in seconds

--- api.js
+++ api.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn;
 
-function callWorker(argument) {
-    let id = Symbol(argument);
-    console.time(id.toString());
+function callWorker(argument, id) {
+    let processStart = new Date();
     let workerCmd = "./worker.py";
     let worker = spawn(workerCmd, [argument]);
     worker.stdout.on('data', (data) => {
-        console.log(`${argument}: ${data.toString()}`);
+        elapsedTime = new Date() - processStart;
+        console.log(`${id}: ${data.toString().trim()}, ${elapsedTime/1000}`);
     });
     worker.stderr.on('data', (data) => {
-        console.error(`${argument}: error`);
+        elapsedTime = new Date() - processStart;
+        console.error(`${id}: error, ${elapsedTime/1000}`);
     });
-    worker.on('close', () => {
-        console.timeEnd(id.toString());
-    });
-
 }
 
 if (process.argv.length > 2) {
     let expressions = process.argv.slice(2);
-    expressions.forEach((expression) => {
-        callWorker(expression);
+    expressions.forEach((expression, index) => {
+        console.log(`${index}: ${expression}`);
+        callWorker(expression, index);
     });
 } else {
     console.error("API error: expected at least one argument");
